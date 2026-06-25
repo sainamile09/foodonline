1e365718478da10ac34b622b2fad6fa30a9e4ef0
saikrishna <saikrishna.bhuvi@gmail.com>
2026-06-25 17:26:54 +0530

    Added user forms and updated templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def registerUser(request):
-    return HttpResponse('user Register successfully')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.Customer
+            user.save()
+            messages.success(request, 'Your account has been created successfully')
+            return redirect('registerUser')
+        else:
+            print('Form is invalid')
+            print(form.errors)
+    else:
+        form = UserForm()
+    
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/registerUser.html',context)
